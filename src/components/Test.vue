b74340798273a9678bf5d2b6b849c5fe0493cc9f
joel <jronceros.byj@gmail.com>
2018-07-22 22:51:39 -0500

    commit inicio

---
+++ src/components/Test.vue
@@ -0,0 +1,67 @@
+<template>
+     <div class="test">
+          <hr>
+          <div v-text="title"></div>
+          <div v-if="showName">
+               <p>{{user.firstName + ' ' + user.lastName}}</p>
+          </div>
+          <div v-else>
+               <p>No puedo mostrar el Componente</p>
+          </div>
+          <hr>
+          <ul>
+               <li v-for="task in tasks">
+                  {{task.title + ' - '}}
+               </li>
+          </ul>
+          <hr>
+          <p> {{ guion() }}  </p>
+     </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      title: "Componente Creado por Joel",
+      user: {
+        firstName: "Joel",
+        lastName: "Ronceros"
+      },
+      myHtml: "<h3>Prueba de Etiqueta</h3>",
+      showName: true,
+      tasks: [
+        { title: "eat" },
+        { title: "dinner" },
+        { title: "play" },
+        { title: "study" }
+      ],
+      guion : function(){
+        
+          for (let index = 0; index < this.tasks.length; index++) {
+              if(this.tasks[3].title == "study"){
+                return this.tasks[3]
+              }
+              
+          }        
+          
+      }
+      
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline;
+  color: #42b983;
+}
+
+</style>
